18f7914f33e7f92a524bacdd2d4b1f362670314e
Konradpina <ksentek@gmx.de>
2023-12-03 09:07:03 +0100

    bug fiexes

--- script/rocket.js
+++ script/rocket.js
@@ -1,6 +1,9 @@
 const rockets= ["rocket1","rocket2","rocket3","rocket4","rocket5"]
 const sparkels=["sparkel1","sparkel2","sparkel3","sparkel4","sparkel5"]
-
+const dotsparks=["botspark1","botspark2","botspark3","botspark4"]
+let r=Math.floor(Math.random()*255)+1
+let g=Math.floor(Math.random()*255)+1
+let b=Math.floor(Math.random()*255)+1
 var rocktime
 var rocktimee
 
@@ -19,9 +22,13 @@ document.getElementById("rocketbox").onmouseleave=e=>{
 function rocketstart(){
     var rocket =document.createElement("span");
     rocket.style.left=`${Math.floor(Math.random()*100)}vw`
-    rocket.style.top=`${250+Math.floor(Math.random()*50)}vh`
+    rocket.style.top=`${250+Math.floor(Math.random()*45)}vh`
     rocket.className=`rocket ${rockets[Math.floor(Math.random()*5)]}`
-    
+   
+    r=Math.floor(Math.random()*255)+1
+    g=Math.floor(Math.random()*255)+1
+    b=Math.floor(Math.random()*255)+1
+
     document.body.appendChild(rocket);
     setTimeout(() => {
         
@@ -31,6 +38,7 @@ function rocketstart(){
 
 
 function rocketdots(){
+     //Start rocket particels
     if(document.getElementsByClassName("rocket").length!==0){
 
         for(i=0;i<document.getElementsByClassName("rocket").length;i++){
@@ -39,6 +47,9 @@ function rocketdots(){
             dot.style.left=`${document.getElementsByClassName("rocket")[i].getBoundingClientRect().x}px`
             dot.style.top=`${document.getElementsByClassName("rocket")[i].getBoundingClientRect().y}px`
             dot.style.marginTop=`200vh`
+            if((Math.random()*100)<20){
+                dot.style.backgroundColor=`rgb(${r},${g},${b})`
+            }
             
             document.body.appendChild(dot);
             setTimeout(() => {
@@ -46,6 +57,8 @@ function rocketdots(){
             }, 1000);
         }   
     }
+
+    //Start Explosion
     if(document.getElementsByClassName("explosion").length!==0){
         
         for(i=0;i<document.getElementsByClassName("explosion").length;i++){
@@ -54,7 +67,7 @@ function rocketdots(){
         const xx=`${document.getElementsByClassName("explosion")[0].getBoundingClientRect().x}px`
         const yy=`${document.getElementsByClassName("explosion")[0].getBoundingClientRect().y}px`
         document.getElementsByClassName("explosion")[0].remove()
-            const colorkey=`rgb(${Math.floor(Math.random()*255)+1},${Math.floor(Math.random()*255)+1},${Math.floor(Math.random()*255)+1})`
+            
             for(i=0;i<points.length;i++){
                 var point =document.createElement("span");
                 document.body.appendChild(point);
@@ -62,11 +75,6 @@ function rocketdots(){
                 point.style.left=xx
                 point.style.top=yy
                 point.className =`point ${points[i]}`
-                point.style.backgroundColor=colorkey
-                point.style.boxShadow=`${colorkey} 0px 0px 2vh`
-                
-                
-               
 
             }
             setTimeout(() => {
@@ -78,19 +86,23 @@ function rocketdots(){
        
     }
 
+    //Render Explosion
     if(document.getElementsByClassName("point").length!==0){
+            let r2=r+(Math.floor(Math.random()*50)-10)
+            let b2=b+(Math.floor(Math.random()*50)-10)
+            let g2=g+(Math.floor(Math.random()*50)-10)
+
 
         for(i=0;i<document.getElementsByClassName("point").length;i++){
-            var dot =document.createElement("span")
-            dot.className =`rockdot ${dots[Math.floor(Math.random()*4)]}`
-            dot.style.left=`${document.getElementsByClassName("point")[i].getBoundingClientRect().x}px`
-            dot.style.top=`${document.getElementsByClassName("point")[i].getBoundingClientRect().y}px`
-            dot.style.marginTop=`200vh`
+            var dod =document.createElement("span")
+            dod.className =`rockdot ${dotsparks[Math.floor(Math.random()*4)]}`
+            dod.style.left=`${document.getElementsByClassName("point")[i].getBoundingClientRect().x}px`
+            dod.style.top=`${document.getElementsByClassName("point")[i].getBoundingClientRect().y}px`
+            dod.style.marginTop=`200vh`
+            dod.style.backgroundColor=`rgb(${r2},${g2},${b2})`
+            dod.style.boxShadow=`rgb(${250},${g},${b}) 0px 0px 2vh`
+            document.body.appendChild(dod);
             
-            document.body.appendChild(dot);
-            setTimeout(() => {
-            dot.remove()
-            }, 1000);
         }   
         setTimeout(() => {
             for(i=0;i<document.getElementsByClassName("rockdot").length;i){
